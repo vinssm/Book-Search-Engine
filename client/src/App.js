9c97467f141f7582d9c1a113888ab0797be970b6
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-03-27 15:59:02 -0400

    deploying server and client

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,44 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 function App() {
+
+  const httpLink = createHttpLink({
+    uri: "/graphql"
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link:authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  })
+
+
+
   return (
-    <Router>
-      <>
+    <ApolloProvider client={client}>
+      <Router>
         <Navbar />
         <Switch>
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
-      </>
-    </Router>
+      </Router>
+    </ApolloProvider>
+    
   );
 }
 
