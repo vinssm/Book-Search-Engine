85c332034fe431df4e58492fa044a3685d0f4749
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-03-28 13:08:41 -0400

    deploying to Heroku

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,25 +15,17 @@ type Book{
     _id: ID
     username: String
     email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
+    bookCount: Int
+    savedBooks: [Book]
   }
 
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
   }
 
   type Auth {
@@ -43,18 +35,13 @@ type Book{
 
   type Query {
     me: User
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addReaction(thoughtId: ID!, reactionBody: String!): Thought
-    addFriend(friendId: ID!): User
+    saveBook(input: BookInput): User
+    removeBook(bookId: String!): User
   }
 `;
 
